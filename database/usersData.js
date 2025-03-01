6dcf0f14f4e1709c558d181e8f8f1ee784f8a0a8
תהילה בן משה <tbenmosh@g.jct.ac.il>
2025-03-01 23:07:32 +0200

    pushhhhhhh

--- database/usersData.js
+++
@@ -1,8 +0,0 @@
-// נתונים ראשוניים של משתמשים
-const users = [
-    { id: 1, username: "admin", password: "1234" },
-    { id: 2, username: "user1", password: "abcd" }
-];
-
-// ייצוא הנתונים לשימוש ב-API
-export default users;
